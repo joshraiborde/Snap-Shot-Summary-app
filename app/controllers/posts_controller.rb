a1052f5fa225ee042cf45df692cbc6ce3dfe5462
joshsrai <joshsrai@gmail.com>
2020-11-21 18:07:38 -0500

    created a CREATE route in post_controller, create a show route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,6 @@ class PostsController < ApplicationController
     # CRUD
 
     # READ
-
     #index route for all posts
     get '/posts' do
         # leverae the model to get all posts from database
@@ -13,8 +12,23 @@ class PostsController < ApplicationController
         erb :'posts/index'
     end
 
-    #show route for a single post
+    # CREATE
+    # render a form to create a new post
+    get '/posts/new' do
+        erb :"posts/new"
 
+    end
+
+    # need another route for create
+    post "/posts" do
+        #recieve the params that the user inputs in the create new form
+        post = Post.create(title: params[:title], image_url: params[:image_url], description: params[:description], user_id: current_user.id)
+        #redirect to the post show page
+        redirect "/posts/#{post.id}"
+    end
+
+    # ORDER MATTERS
+    #show route for a single post
     get '/posts/:id' do
         # find the post
         @post = Post.find(params[:id])
@@ -23,10 +37,10 @@ class PostsController < ApplicationController
 
 
 
-    # CREATE
-
-
     # UPDATE
+    # edit button that routes to the form
+    # render an edit form
+    # patch method/route that will update the post
 
 
     #DELETE
