451ffb61ff7884b52d0e55510a3acd8b10c48916
joshsrai <joshsrai@gmail.com>
2020-11-21 10:23:30 -0500

    welcomepageedit, logout, layout, login, signup, userspage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,33 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     #enables the sessions hash
     enable :sessions
+    #enables the user to login
     #set a session secret for an extra layer of security
     set :session_secret, "super_secret_session"
   end
 
   get "/" do
+    if logged_in?
+      redirect "/users/#{current_user.id}"
+    else
     erb :welcome
+    end
+  end
+
+  helpers do
+
+    # returns a boolean if the user is logged in or not
+     def logged_in?
+      !!current_user
+     end
+
+
+    # keeps track of the logged in user
+    def current_user
+      User.find_by(id: session[:user_id])
+      #User.find(session[:user_id])
+      #allows us to leverage the login user anywhere where in the controllers where it is inheriting from the application controller
+    end
   end
 
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,13 +8,13 @@ class UserController < ApplicationController
     # reveive the data (params) from the login form
     post "/login" do
         # find the user
-        user = User.find_by(email: params[:email])
+        @user = User.find_by(email: params[:email])
         # authenticate the user
-        if user && user.authenticate(params[:password])# if this user is truthy and if the user has been authenticated, then log them in, the authenticate method comes from snap_shot_summary_app/app/models/user.rb file's 'has_secure_password'.
+        if @user && @user.authenticate(params[:password])# if this user is truthy and if the user has been authenticated, then log them in, the authenticate method comes from snap_shot_summary_app/app/models/user.rb file's 'has_secure_password'.
         # creating a key/value pair in the session hash using the users id to actually logs them in
-        session[:user_id] = user.id
+        session[:user_id] = @user.id
         # redirect user's profile (users show)
-        redirect "/users/#{user.id}"
+        redirect "/users/#{@user.id}"
         else
             # show an error message
             redirect '/login'
@@ -23,7 +23,42 @@ class UserController < ApplicationController
 
     # users SHOW route
     get "/users/:id" do
-        "users show page!"
+        # binding.pry
+        @user = User.find_by(id: params[:id])
+        erb :'/users/show'
+    end
+
+    # SIGN UP
+    # get sign up route that renders signup form
+    get '/signup' do
+        #render sign up form
+        erb :signup
+    end
+
+    # post sign up route that recieve input data from user, create theuser, and logs user in
+    post '/users' do
+        @user = User.new(params)
+        if @user.save
+          #creating a session, adding a key/value pair to session hash
+          session[:user_id] = @user.id
+          # redirect to user profile
+          redirect "/users/#{@user.id}"
+        else
+          #not valid input
+          redirect '/signup'
+        end
+      end
+
+
+
+    # LOG OUT
+    # get logout that clears the session hash
+    get '/logout' do
+        # binding.pry
+        session.clear
+        # binding.pry
+        # redirect to home page
+        redirect '/'
     end
 
 end
